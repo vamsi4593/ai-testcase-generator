c45d3c78dba4327adca191dc19007508b575f6c8
vamsi4593 <vamsikrishnapanganamala@gmail.com>
2026-03-17 18:49:23 +0530

    feat: add RAG-based retrieval using FAISS for context-aware test case genration

---
+++ rag/documents.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sympy import expand
+
+
+def document_writer(test_case_type):
+    file_path = "C:/Users/LENOVO/testcases/payment_testcases_formatted.csv"
+
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_colwidth', None)
+
+    df = pd.read_csv(file_path).ffill()
+
+    groups = df.groupby(["test_case_id", "title", "test_type"])
+
+    result = (
+        df.groupby(["test_case_id", "title","test_type"])
+        .apply(lambda x: x.to_dict("records"),
+               include_groups=False)
+        .to_dict())
+
+    #result = {f'{test_case_id} - {title}': rows for (test_case_id, title), rows in result.items()}
+
+    documents = []
+    for key in result.keys():
+        doc_str = f"test case id : {key[0]}\ntitle : {key[1]}\ntest type : {key[2]}\n"
+        expected_result = ""
+        for row in result[key]:
+            for row_key in row:
+                if row_key == "step_number" or row_key == "action":
+                    continue
+                expected_result = f"{row[row_key]}"
+        doc_str += f"expected result : {expected_result}\n"
+        documents.append(doc_str)
+    print(f"length of documents : {len(documents)}")
+    filtered_doc = [doc for doc in documents if test_case_type in doc]
+    print(f"length of filtered documents : {len(filtered_doc)}")
+    return filtered_doc
+
